66d5d5ecf2a6cd4dcfb264e220f9e944884b29d3
hb <hb@aroma-project.org>
2014-08-07 22:23:10 +0200

    Added test for 'groupBy' argument to Bowtie2Alignment.

---
+++ tests/Bowtie2Alignment,PE,groupBy.R
@@ -0,0 +1,55 @@
+library("aroma.seq")
+setOption("R.filesets/parallel", "none")
+
+fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
+fullTest <- fullTest && isCapableOf(aroma.seq, "bowtie2")
+if (fullTest) {
+
+dataset <- "YeastTest"
+organism <- "SaccharomycesCerevisiae"
+
+# Setup (writable) local data directory structure
+setupExampleData()
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Annotation data
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fa <- FastaReferenceFile$byOrganism(organism)
+print(fa)
+
+# Bowtie2 index set
+is <- buildBowtie2IndexSet(fa, verbose=TRUE)  # is = 'index set'
+print(is)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# FASTQ data
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fqs <- FastqDataSet$byName(dataset, organism=organism, paired=TRUE)
+print(fqs)
+
+# Set fullnames translator, making SRR + 5 digits the name
+# and the rest tags, just as an example
+fqs <- setFullNamesTranslator(fqs, function(names, ...) {
+  # Drop any stray "R1" suffix
+  names <- gsub("_(1|R1)$", "", names)
+  # Tagify
+  gsub("_", ",", names, fixed=TRUE)
+})
+print(getFullNames(fqs))
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# TopHat alignment
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ta <- Bowtie2Alignment(dataSet=fqs, groupBy="name", indexSet=is)
+print(ta)
+
+# Assert that for each group a unique set of files are identified
+groups <- getGroups(ta)
+lapply(groups, FUN=function(idxs) stopifnot(!anyDuplicated(idxs)))
+
+bams <- process(ta, verbose=-100)
+print(bams)
+} # if (fullTest)
