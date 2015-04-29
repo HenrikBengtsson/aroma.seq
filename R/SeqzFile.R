70865ab08a2c0e7597e758dc62aab1a3b4574e37
hb <hb@aroma-project.org>
2015-04-29 16:32:53 -0700

    Forgot to add SeqzFile

---
+++ R/SeqzFile.R
@@ -0,0 +1,38 @@
+setConstructorS3("SeqzFile", function(..., .verify=FALSE) {
+  extend(TabularTextFile(..., .verify=.verify), "SeqzFile")
+})
+
+setMethodS3("getDefaultColumnClassPatterns", "SeqzFile", function(this, ...) {
+  c("*"="NULL", "(chromosome|base.ref|AB.|zygosity.normal|tumor.strand)"="character", "(position|depth.normal|depth.tumor|good.reads)"="integer", "(Af|Bf)|GC.percent"="double", "ratio"="NULL")
+}, protected=TRUE)
+
+
+setMethodS3("readAnnotationData", "SeqzFile", function(this, colClasses=c(chromosome="character", position="integer", GC.percent="numeric", base.ref="character"), ...) {
+  readDataFrame(this, colClasses=colClasses, ...)
+})
+
+
+setMethodS3("readTotalCNsAndBAFs", "SeqzFile", function(this, ploidy=2, ...) {
+  data <- readDataFrame(this, colClasses=c(chromosome="character", position="integer", "(depth.normal|depth.tumor)"="integer", "(Af|Bf)"="numeric", zygosity.normal="character"), ...)
+  depth <- data$depth.tumor + data$depth.normal
+  tcn <- ploidy * data$depth.tumor / data$depth.normal
+  data <- data.frame(chromosome=data$chromosome, x=data$position, depth=depth, tcn=tcn, baf=data$Bf, isHet=(data$zygosity.normal == "het"), stringsAsFactors=FALSE)
+  data
+})
+
+
+setConstructorS3("SeqzFileSet", function(...) {
+  extend(TabularTextFileSet(...), "SeqzFileSet")
+})
+
+setMethodS3("findByName", "SeqzFileSet", function(static, ..., pattern="[.]seqz$", paths=c("seqzData")) {
+  NextMethod("findByName", paths=paths, pattern=pattern)
+}, static=TRUE)
+
+setMethodS3("byName", "SeqzFileSet", function(static, ..., paths=c("seqzData")) {
+  NextMethod("byName", paths=paths)
+}, static=TRUE)
+
+setMethodS3("readAnnotationData", "SeqzFileSet", function(this, colClasses=c(chromosome="character", position="integer", GC.percent="numeric", base.ref="character"), ...) {
+  readDataFrame(this, colClasses=colClasses, ...)
+})
