90c4e6585c74b6de3edee46546e54d6d52965650
hb <hb@aroma-project.org>
2015-05-06 15:01:33 -0700

    TESTS: Added test script for BamDataFileSet and BamDataFile

---
+++ tests/BamDataFileSet.R
@@ -0,0 +1,19 @@
+library("aroma.seq")
+setOption("R.filesets/parallel", "none")
+
+fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
+fullTest <- fullTest && isCapableOf(aroma.seq, "bowtie2")
+if (fullTest) {
+
+setupExampleData()
+dataSet <- "TopHat-example"
+organism <- "LambdaPhage"
+fa <- FastaReferenceFile$byOrganism(organism)
+fqs <- FastqDataSet$byName(dataSet, organism=organism)
+bams <- doBowtie2(fqs, reference=fa, verbose=-20)
+print(bams)
+
+bam <- bams[[1]]
+print(bam)
+
+} # if (fullTest)
