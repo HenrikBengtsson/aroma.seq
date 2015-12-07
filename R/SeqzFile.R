589402dc2d47473d2d3b0f74a3ec7e95df29c7fd
hb <hb@aroma-project.org>
2015-12-07 13:25:20 -0800

    Now SeqzFile also supports gzip'ed files

--- R/SeqzFile.R
+++ R/SeqzFile.R
@@ -2,6 +2,20 @@ setConstructorS3("SeqzFile", function(..., .verify=FALSE) {
   extend(TabularTextFile(..., .verify=.verify), "SeqzFile")
 })
 
+setMethodS3("getDefaultFullName", "SeqzFile", function(this, ...) {
+  fullname <- NextMethod("getDefaultFullName")
+
+  ## Support gzipped files
+  ## FIXME: Can't this be done by TabularTextFile? /HB 2015-12-07
+  if (isGzipped(this)) {
+    pattern <- getExtensionPattern(this)
+    fullname <- gsub(pattern, "", fullname)
+  }
+
+  fullname
+}, protected=TRUE)
+
+
 setMethodS3("getDefaultColumnClassPatterns", "SeqzFile", function(this, ...) {
   c("*"="NULL", "(chromosome|base.ref|AB.|zygosity.normal|tumor.strand)"="character", "(position|depth.normal|depth.tumor|good.reads)"="integer", "(Af|Bf)|GC.percent"="double", "ratio"="NULL")
 }, protected=TRUE)
@@ -25,7 +39,7 @@ setConstructorS3("SeqzFileSet", function(...) {
   extend(TabularTextFileSet(...), "SeqzFileSet")
 })
 
-setMethodS3("findByName", "SeqzFileSet", function(static, ..., pattern="[.]seqz$", paths=c("seqzData")) {
+setMethodS3("findByName", "SeqzFileSet", function(static, ..., pattern="[.]seqz(|[.]gz)$", paths=c("seqzData")) {
   NextMethod("findByName", paths=paths, pattern=pattern)
 }, static=TRUE)
 
