54a6ca80ee5e4cbff9f6705547953fa806167ff6
hb <hb@aroma-project.org>
2015-12-11 10:41:35 -0800

    Add getDefaultFullName() for MPileupFile

--- R/MPileupFile.R
+++ R/MPileupFile.R
@@ -2,6 +2,24 @@ setConstructorS3("MPileupFile", function(...) {
   extend(GenericDataFile(...), "MPileupFile")
 })
 
+setMethodS3("getDefaultFullName", "MPileupFile", function(this, ...) {
+  fullname <- NextMethod("getDefaultFullName")
+
+  ## Support gzipped files
+  ## FIXME: Can't this be done by TabularTextFile? /HB 2015-12-07
+  if (isGzipped(this)) {
+    pattern <- getExtensionPattern(this)
+    fullname <- gsub(pattern, "", fullname)
+  }
+
+  fullname
+}, protected=TRUE)
+
+
 setConstructorS3("MPileupFileSet", function(...) {
   extend(GenericDataFileSet(...), "MPileupFileSet")
 })
+
+setMethodS3("findByName", "MPileupFileSet", function(static, ..., pattern="[.]mpileup(|[.]gz)$", paths=c("mpileupData")) {
+  NextMethod("findByName", paths=paths, pattern=pattern)
+}, static=TRUE)
