313dbe777cbbf8fcd80fe26f8a495c1e641880ce
hb <hb@aroma-project.org>
2016-01-06 14:01:32 -0800

    TESTS: Add explicit test on buildBwaIndexSet() [#33]

---
+++ tests/BwaIndexSet.R
@@ -0,0 +1,22 @@
+library("aroma.seq")
+
+fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
+fullTest <- fullTest && isCapableOf(aroma.seq, "bwa")
+if (fullTest) {
+
+## Setup (writable) local data directory structure
+setupExampleData()
+
+dataset <- "YeastTest"
+organism <- "Saccharomyces_cerevisiae"
+
+## Setup FASTA reference file
+fa <- FastaReferenceFile$byOrganism(organism)
+print(fa)
+
+## Build index set
+is <- buildBwaIndexSet(fa, verbose=-10)
+print(is)
+
+
+} # if (fullTest)
