f248c5c56748eca0b0822615004467f2dd3deb9c
hb <hb@aroma-project.org>
2016-01-06 14:02:01 -0800

    Add getSeqLengths(), getSeqNames() etc for BwaIndexSet [#33]

--- R/BwaIndexSet.R
+++ R/BwaIndexSet.R
@@ -1,5 +1,9 @@
 ###########################################################################/**
 # @RdocClass BwaIndexSet
+# @aliasmethod isComplete
+# @aliasmethod getSeqLengths
+# @aliasmethod getSeqNames
+# @aliasmethod readAnnData
 #
 # @title "The BwaIndexSet class"
 #
@@ -38,6 +42,56 @@ setMethodS3("isComplete", "BwaIndexSet", function(this, ...) {
   }
 
   TRUE;
+})
+
+
+setMethodS3("getSeqLengths", "BwaIndexSet", function(this, ...) {
+  data <- readAnnData(this)
+  lengths <- data$length
+  names(lengths) <- data$name
+  lengths
+})
+
+
+setMethodS3("getSeqNames", "BwaIndexSet", function(this, ...) {
+  names(getSeqLengths(this))
+})
+
+
+setMethodS3("readAnnData", "BwaIndexSet", function(this, ...) {
+  ## Identify sequence names and lengths
+  pathnames <- getPathnames(this)
+  ann <- is[[grep("[.]ann$", pathnames)]]
+
+  pathname <- getPathname(ann)
+  data <- readLines(pathname)
+
+  ## Parse
+  dataT <- lapply(data, FUN=function(x) {
+    x <- gsub("^([^ ]+) ([^ ]+) (.+)", "\\1\t\\2\t\\3", x)
+    strsplit(x, split="\t", fixed=TRUE)[[1]]
+  })
+  ns <- sapply(dataT, FUN=length)
+  stopifnot(all(ns == 3))
+  hdr <- dataT[[1]]
+  hdr <- list(
+    totalSeqLength=as.double(hdr[1]),
+    nbrOfSequences=as.integer(hdr[2]),
+    unknown=hdr[3]
+  )
+  dataT <- dataT[-1]
+  stopifnot(length(dataT) == 2*hdr$nbrOfSequences)
+  seqInfo <- list()
+  for (kk in seq_len(hdr$nbrOfSequences)) {
+    dataKK <- dataT[1:2 + 2*(kk-1)]
+    dataKK <- unlist(dataKK, use.names=FALSE)
+    seqInfoKK <- data.frame(a=dataKK[1], name=dataKK[2], description=dataKK[3], start=dataKK[4], length=as.double(dataKK[5]), c=dataKK[6], stringsAsFactors=FALSE)
+    seqInfo[[kk]] <- seqInfoKK
+  }
+  seqInfo <- Reduce(rbind, seqInfo)
+  stopifnot(sum(seqInfo$length) == hdr$totalSeqLength)
+  attr(seqInfo, "header") <- hdr
+  seqInfo
 })
 
 
