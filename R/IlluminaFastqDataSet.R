1ce1e7db9f1cbcd3f446ef5f9b0a99cf9ef9a050
hb <hb@aroma-project.org>
2016-02-17 20:30:06 -0800

    ROBUSTNESS: Now the constructor of IlluminaFastqDataFile asserts that all FASTQ files have a file format that is recognized/known.

--- R/IlluminaFastqDataSet.R
+++ R/IlluminaFastqDataSet.R
@@ -1,3 +1,4 @@
+
 ###########################################################################/**
 # @RdocClass IlluminaFastqDataSet
 #
@@ -23,8 +24,32 @@
 # @author "HB"
 #*/###########################################################################
 setConstructorS3("IlluminaFastqDataSet", function(...) {
-  extend(FastqDataSet(...), "IlluminaFastqDataSet");
+  this <- extend(FastqDataSet(...), "IlluminaFastqDataSet")
+  validate(this)
 })
+
+
+setMethodS3("as.character", "IlluminaFastqDataSet", function(x, ...) {
+  s <- NextMethod("as.character")
+  s <- c(s, sprintf("Platform: %s", htable(sapply(x, FUN=getPlatform))))
+  s <- c(s, sprintf("File format version: %s", htable(sapply(x, FUN=getFileVersion))))
+  s
+}, protected=TRUE)
+
+
+setMethodS3("validate", "IlluminaFastqDataSet", function(this, ...) {
+  NextMethod("validate")
+  for (ii in seq_along(this)) {
+    fq <- this[[ii]]
+    if (!inherits(fq, "IlluminaFastqDataFile")) {
+      throw(sprintf("Invalid %s: Detected non-IlluminaFastqDataFile (of class %s): %s", class(this)[1], class(fq)[1], getPathname(fq)))
+    }
+    if (is.na(getFileVersion(fq))) {
+      throw(sprintf("Invalid %s: Detected %s with unknown file format version: %s", class(this)[1], class(fq)[1], getPathname(fq)))
+    }
+  }
+  invisible(this)
+}, protected=TRUE)
 
 
 ############################################################################
